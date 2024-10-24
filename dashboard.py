1cda9fff0843076435de5379c081bd4b2ac877dc
microcoded <jacob@goodrid.ge>
2024-10-24 20:15:42 +1100

    Update web interface to support congestion levels

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Sample data
+study_areas = [
+    {'name': 'Library Level 8', 'congestion_level': 'low'},
+    {'name': 'CB11.04.400', 'congestion_level': 'medium'},
+    {'name': 'Reading Room', 'congestion_level': 'high'},
+    {'name': 'FEIT FLP', 'congestion_level': 'medium'}
+]
+
 @app.route('/')
-def webpage():
-    return render_template('index.html')
+def dashboard():
+    return render_template('dashboard.html', study_areas=study_areas)
