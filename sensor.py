477581924d19eab7523a25ce276d4a9ba4cbff57
Liam <liam.indiana.brown@gmail.com>
2024-10-24 19:54:50 +1100

    Modified code to work with bleak library instead of simplepyble

--- sensor.py
+++ sensor.py
@@ -1,32 +1,39 @@
-import simplepyble
+import asyncio
+from bleak import BleakScanner
 
-if __name__ == "__main__":
-    adapter = simplepyble.Adapter.get_adapters()[0]
+DEVICE_COUNT = 0
 
-    print(f"Selected adapter: {adapter.identifier()} [{adapter.address()}]")
 
-    adapter.set_callback_on_scan_start(lambda: print("Scan started."))
-    adapter.set_callback_on_scan_stop(lambda: print("Scan complete."))
+def simple_callback(device, advertisement_data):
+    global DEVICE_COUNT
+    DEVICE_COUNT += 1
+    name = advertisement_data.local_name if advertisement_data.local_name else None
+    print(f"{device.address}: {name}")
 
-    # Scan for 5 seconds
-    adapter.scan_for(5000)
+    print(f"    Tx Power: {advertisement_data.rssi} dBm")
 
-    peripherals = adapter.scan_get_results()
-    print("The following peripherals were found:")
-    for peripheral in peripherals:
-        connectable_str = "Connectable" if peripheral.is_connectable() else "Non-Connectable"
-        print(f"{peripheral.identifier()} [{peripheral.address()}] - {connectable_str}")
-        print(f'    Address Type: {peripheral.address_type()}')
-        print(f'    Tx Power: {peripheral.tx_power()} dBm')
-
-        manufacturer_data = peripheral.manufacturer_data()
-        for manufacturer_id, value in manufacturer_data.items():
+    if advertisement_data.manufacturer_data:
+        for manufacturer_id, value in advertisement_data.manufacturer_data.items():
             print(f"    Manufacturer ID: {manufacturer_id}")
             print(f"    Manufacturer data: {value.hex()}")
 
-        services = peripheral.services()
-        for service in services:
-            print(f"    Service UUID: {service.uuid()}")
-            print(f"    Service data: {service.data().hex()}")
+    if advertisement_data.service_uuids:
+        print(f"    Service UUID: {advertisement_data.service_uuids}")
+    if advertisement_data.service_data:
+        print(f"    Service data: {advertisement_data.service_data}")
 
-    print(f"A total of {len(peripherals)} were found.")
+
+async def main():
+    scanner = BleakScanner(
+        simple_callback
+    )
+    global DEVICE_COUNT
+    for i in range(0, 5):
+        print("(re)starting scanner")
+        async with scanner:
+            await asyncio.sleep(5.0)
+        print(f"Devices in scan: {DEVICE_COUNT}")
+        DEVICE_COUNT = 0
+
+
+asyncio.run(main())
